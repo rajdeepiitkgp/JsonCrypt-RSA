585ed90e18dac7240d53700c6d35925713044b11
rajdeepiitkgp <rajdeep.biswas2010@gmail.com>
2020-11-15 21:19:16 +0530

    updating Path

--- src/__tests__/bigjson.test.ts
+++ src/__tests__/bigjson.test.ts
@@ -2,10 +2,10 @@ import { CreateCryptDecryptObject } from './ObjectCreater';
 import fs from 'fs';
 
 test('Test A Very Big JSON Data', () => {
-    const { objCrypter, objDecrypter } = CreateCryptDecryptObject();
-    const inputObj = JSON.parse(fs.readFileSync('./bigjson.json','utf-8')) as object ;
-    const encryptedObject = objCrypter.EncryptJson(inputObj);
-    const outputObject = objDecrypter.DecryptJson<object>(encryptedObject);
-    expect(inputObj).toMatchObject(outputObject);
-    expect(outputObject).toMatchObject(inputObj);
-  });
+  const { objCrypter, objDecrypter } = CreateCryptDecryptObject();
+  const inputObj = JSON.parse(fs.readFileSync('bigjson.json', 'utf-8')) as object;
+  const encryptedObject = objCrypter.EncryptJson(inputObj);
+  const outputObject = objDecrypter.DecryptJson<object>(encryptedObject);
+  expect(inputObj).toMatchObject(outputObject);
+  expect(outputObject).toMatchObject(inputObj);
+});
